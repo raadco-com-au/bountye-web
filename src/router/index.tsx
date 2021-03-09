a074b8e04bbb74e4e11e3b6bfdbf40d57dec5391
Mohsen <mohsen@vanishingpoint.com.au>
2021-03-09 17:38:30 +0330

    Update index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,29 +1,22 @@
-import * as React from 'react';
-import {
-    Router, Route, Switch,Link
-} from 'react-router-dom';
+import * as React from "react";
+import { Router, Route, Switch, Link } from "react-router-dom";
 import Main from "./main.route";
 // import Panel from "./panel.route";
+import history from "./history";
 
-import history from './history';
-
- 
-
-class Index extends React.PureComponent<{}>{
-
-    render() {
-        return (
-            <Router history={history}>
-                <React.Fragment>
-                    <Switch>
-                        
-                        {null == null && <Route  path={'/*'} component={Main} />}   
-                        {/* {getToken() == null &&  <Route exact path={'/*'} component={Main} />} */}
-                    </Switch>
-                </React.Fragment>
-            </Router>
-        );
-    }
+class Index extends React.PureComponent<{}> {
+  render() {
+    return (
+      <Router history={history}>
+        <React.Fragment>
+          <Switch>
+            {null == null && <Route path={"/*"} component={Main} />}
+            {/* {getToken() == null &&  <Route exact path={'/*'} component={Main} />} */}
+          </Switch>
+        </React.Fragment>
+      </Router>
+    );
+  }
 }
 
 export default Index;
